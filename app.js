1fa79db2fc5ddd3373c81c530e634c474d48c829
Jean Ha <jeanha124@gmail.com>
2020-02-28 08:23:08 -0500

    'Setup mongoose'

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 //Creates new Express server
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true})
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 //port to run the server -> server will run on localhost:5000
 const port = process.env.PORT || 5000;
 //Basic route
