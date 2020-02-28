059aaa0c80b8f6286daaed1b1ef796096e7904e4
Jean Ha <jeanha124@gmail.com>
2020-02-28 08:15:03 -0500

    "Use nodemon"

--- app.js
+++ app.js
@@ -8,3 +8,7 @@ app.get('/', (req, res) => res.send('Hello World'));
 
 //tell Express to start a socket and listen for connections on the path
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+//run node app and open localhost:5000
+//to user nodemon, run npm run start to get same result
+//user npm run server to get changes to log on the server
