52a25b6a7b7d7f1e4e010f333d600cff74735971
Jean Ha <jeanha124@gmail.com>
2020-02-28 08:30:10 -0500

    'Express routing and testing'

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const app = express();
 //connect to database
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
+//import routes
+const users = require('./routes/api/users');
+const tweets = require('./routes/api/tweets');
+
+const bodyParser = require('body-parser');
 
 mongoose
   .connect(db, { useNewUrlParser: true})
@@ -20,3 +25,10 @@ app.listen(port, () => console.log(`Server is running on port ${port}`));
 //run node app and open localhost:5000
 //to user nodemon, run npm run start to get same result
 //user npm run server to get changes to log on the server
+
+//tell Express to use new imported routes
+app.use('/api/users', users);
+app.use('/api/tweets', tweets);
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
